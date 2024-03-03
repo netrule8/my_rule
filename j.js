64150a422b169c0d9ea8a93a166bcf653188494f
Scott Judson <79251671+netrule8@users.noreply.github.com>
2024-03-03 13:56:28 +0800

    Update j.js

--- j.js
+++ j.js
@@ -1,7 +1,7 @@
 [rewrite_local]
   
 # > 财新周刊☆解锁阅读权限（2023-08-02）@ddgksf2013
-http?:\/\/app\.qikan\.cn\/artical\/detail url script-response-body https://github.com/netrule8/my_rule/raw/master/j.js
+http?:\/\/app\.qikan\.cn\/artical\/detail url script-response-body https://github.com/netrule8/my_rule/raw/main/j.js
 
 
 [mitm] 
