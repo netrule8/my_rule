630ec04188c3d1223d18d4be11468af0cefde92d
Scott Judson <79251671+netrule8@users.noreply.github.com>
2024-03-03 14:22:27 +0800

    Update j.js

--- j.js
+++ j.js
@@ -1,14 +1,2 @@
-[rewrite_local]
-http:\/\/app\.qikan\.cn\/artical\/detail url script-response-body https://github.com/netrule8/my_rule/raw/main/j.js
-
-
-[mitm] 
-
-hostname=app.qikan.cn
-
-
-
-
-
 var body = $response.body.replace(/isBuy":0/g, 'isBuy":1')
 $done({ body })
