ee3b673c25486f0e05aa61c5b3892d571833d422
Scott Judson <79251671+netrule8@users.noreply.github.com>
2024-03-03 14:03:30 +0800

    Update j.js

--- j.js
+++ j.js
@@ -1,14 +1,10 @@
 [rewrite_local]
-  
-# > 财新周刊☆解锁阅读权限（2023-08-02）@ddgksf2013
 http?:\/\/app\.qikan\.cn\/artical\/detail url script-response-body https://github.com/netrule8/my_rule/raw/main/j.js
 
 
 [mitm] 
 
 hostname=app.qikan.cn
-
-***********************************/
 
 
 
